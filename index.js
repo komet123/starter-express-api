948cf790b889656f39685346e1c210a692676c95
Master 123 <54767714+komet123@users.noreply.github.com>
2023-02-24 20:31:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all('/home', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Halo zidan')
+})
+
 app.listen(process.env.PORT || 3000)
